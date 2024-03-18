6b1439361c94f66e421ba7abf5eeddf670e4204d
Beareney <126513786+Beareney@users.noreply.github.com>
2024-03-18 00:13:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+      """
+    Returns:
+        str: Contenido de la plantilla HTML '/en/index.html'.
+    """
     return render_template('/en/index.html')
 
 if __name__ == '__main__':
